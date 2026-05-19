cfe7e6cbd099c1931da98ab5beccf8c7e5669b84
naxsyambis <naxsyambis@gmail.com>
2026-05-19 14:37:21 +0700

    feat: implementasi entitas Profile dengan pemetaan JPA

--- src/main/java/com/deploy/pertemuan11/model/Profile.java
+++ src/main/java/com/deploy/pertemuan11/model/Profile.java
@@ -1,4 +1,28 @@
 package com.deploy.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
+
+	@Id
+	private String id;
+
+	private String nama;
+
+	private String alamat;
+
+	@OneToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
 }
